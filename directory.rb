ea150ae90268c6d4868e18e9f48929edc6579a1d
Daniel Ridgway <ridginald@gmail.com>
2018-05-10 13:30:52 +0100

    save11

--- directory.rb
+++ directory.rb
@@ -23,6 +23,7 @@ end
   def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list of students.csv"
     puts "9. Exit" # 9 because we'll be adding more items
   end
 
@@ -38,6 +39,8 @@ end
           input_students
         when "2"
           show_students
+        when "3"
+          save_students
         when "9"
           exit # this will cause the program to terminate
         else
@@ -57,5 +60,17 @@ end
 def print_footer(students)
   puts "Overall, we have #{@students.count} great students"
 end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end 
+
 #nothing happens until we call the methods
 interactive_menu
